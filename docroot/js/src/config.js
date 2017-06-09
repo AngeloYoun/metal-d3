a2664aba7713c8d781400272b1447f9327703b47
Fernando Souza <nandosouzafilho@gmail.com>
2017-06-09 15:28:19 -0700

    Adds config file

---
+++ docroot/js/src/config.js
@@ -0,0 +1,10 @@
+export const BASE_HEIGHT = 500;
+export const BASE_WIDTH = 960;
+export const margin = {
+	bottom: 30, 
+	left: 20,
+	right: 40, 
+	top: 20
+}
+export const WIDTH = BASE_WIDTH - margin.left - margin.right;
+export const HEIGHT = BASE_HEIGHT - margin.top - margin.bottom;

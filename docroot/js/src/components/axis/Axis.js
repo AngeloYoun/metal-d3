fc556bc54fea0378d14009afca4753f0dd6f334b
Angelo Youn <angelo.youn@liferay.com>
2017-06-13 00:34:59 -0700

    Expand axis functionality

--- docroot/js/src/components/axis/Axis.js
+++ docroot/js/src/components/axis/Axis.js
@@ -1,4 +1,5 @@
 import JSXComponent, {Config} from 'metal-jsx';
+import lodash from 'lodash';
 import * as D3 from 'd3';
 
 const RIGHT = 2;
@@ -7,12 +8,13 @@ const BOTTOM = 3;
 class Axis extends JSXComponent {
     render() {
         const {
+            elementClasses,
             d3Axis,
-            d3Scale,
-            domain,
-            range,
+            scale,
+            tickSize,
             ticks,
             tickFormat,
+            renderer,
             ...otherProps
         } = this.props;
 
@@ -22,28 +24,29 @@ class Axis extends JSXComponent {
     rendered() {
         const {
             d3Axis,
-            d3Scale,
-            domain,
-            range,
+            scale,
+            tickSize,
             ticks,
-            tickFormat
+            tickFormat,
+            renderer
         } = this.props;
-
-        const scale = d3Scale().range(range).domain(domain);
         
         const axis = d3Axis(scale)
+            .tickSize(tickSize)
             .ticks(ticks)
 			.tickFormat(tickFormat)
 
-        axis(
-            D3.select(this.refs.axis)
-        );
+        D3.select(this.refs.axis).call(axis).call(renderer)
     }
 }
 
 Axis.PROPS = {
     range: Config.array().value([]),
     domain: Config.array().value([]),
+    tickSize: Config.number().value(6),
+    ticks: Config.value(10),
+    tickFormat: Config.func().value(f => f),
+    renderer: Config.func().value(f => f),
 }
 
 export const RightAxis = class RightAxis extends JSXComponent {

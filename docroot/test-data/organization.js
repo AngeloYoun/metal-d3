d81d8de0070a22dd32ff635e0662af6e3c91f5f4
Angelo Youn <angelo.youn@liferay.com>
2017-06-14 10:51:08 -0700

    Add test data of OrgChart

---
+++ docroot/test-data/organization.js
@@ -0,0 +1,323 @@
+export default {
+    division: {
+        name: 'Liferay',
+        childDivisions: [
+            {
+                name: 'Engineering',
+                childDivisions: [
+                    {
+                        name: 'Quality Assurance',
+                        id: 3
+                    },
+                    {
+                        name: 'Frontend Engineering',
+                        id: 4
+                    },
+                    {
+                        name: 'Knowledge Management',
+                        id: 5
+                    },
+                    {
+                        name: 'Backend Development Assurance',
+                        id: 6
+                    },
+                    {
+                        name: 'Engineering Project Management',
+                        id: 7
+                    },
+                    {
+                        name: 'Mobile Development',
+                        id: 8
+                    },
+                    {
+                        name: 'Developer Relations',
+                        id: 9
+                    },
+                    {
+                        name: 'Engineering - LATAM',
+                        id: 10
+                    },
+                    {
+                        name: 'Engineering - China',
+                        id: 11
+                    },
+                    {
+                        name: 'Engineering - EMEA',
+                        id: 12
+                    },
+                    {
+                        name: 'Engineering - NA',
+                        id: 13
+                    }
+                ],
+                id: 2
+            },
+            {
+                name: 'Human Resources',
+                id: 14
+            },
+            {
+                name: 'Marketing',
+                childDivisions: [
+                    {
+                        name: 'Field Marketing',
+                        childDivisions: [
+                            {
+                                name: 'EU Field Marketing'
+                            }
+                        ],
+                        id: 16
+                    },
+                    {
+                        name: 'Global Marketing',
+                        childDivisions: [
+                            {
+                                name: 'Demand Generation',
+                                id: 17
+                            },
+                            {
+                                name: 'Design',
+                                id: 18
+                            },
+                            {
+                                name: 'Events',
+                                id: 19
+                            },
+                            {
+                                name: 'MarCom',
+                                id: 20
+                            },
+                            {
+                                name: 'Marketing Operations',
+                                id: 21
+                            }
+                        ]
+                    }
+                ],
+                id: 15
+            },
+            {
+                name: 'Operations',
+                childDivisions: [
+                    {
+                        name: 'Accounts',
+                        childDivisions: [
+                            {
+                                name: 'China Accounts',
+                                id: 24
+                            },
+                            {
+                                name: 'German Accounts',
+                                id: 25
+                            },
+                            {
+                                name: 'Hungary Accounts',
+                                id: 26
+                            },
+                            {
+                                name: 'International Accounts',
+                                id: 27
+                            },
+                            {
+                                name: 'Latin American Accounts',
+                                id: 28
+                            },
+                            {
+                                name: 'Spain Accounts',
+                                id: 29
+                            },
+                            {
+                                name: 'US Accounts',
+                                id: 30
+                            }
+                        ],
+                        id: 23
+                    },
+                    {
+                        name: 'Information Security',
+                        id: 31
+                    },
+                    {
+                        name: 'Information Services',
+                        childDivisions: [
+                            {
+                                name: 'Internal Tools',
+                                id: 32
+                            },
+                            {
+                                name: 'IS PMO',
+                                id: 33
+                            },
+                            {
+                                name: 'Liferay Support Development',
+                                id: 34
+                            },
+                            {
+                                name: 'Marketplace',
+                                id: 35
+                            },
+                            {
+                                name: 'Sales Development',
+                                id: 36
+                            },
+                            {
+                                name: 'Web Team',
+                                id: 37
+                            }
+                        ],
+                        id: 38
+                    },
+                    {
+                        name: 'Infastructure',
+                        childDivisions: [
+                            {
+                                name: 'Facilities',
+                                id: 40
+                            },
+                            {
+                                name: 'Information Technology',
+                                id: 41
+                            }
+                        ],
+                        id: 39
+                    },
+                    {
+                        name: 'Legal',
+                        id: 42
+                    }
+                ],
+                id: 22
+            },
+            {
+                name: 'Product Management',
+                childDivisions: [
+                    {
+                        name: 'User Experience & Design',
+                        id: 43
+                    }
+                ],
+                id: 44
+            },
+            {
+                name: 'Sales',
+                childDivisions: [
+                    {
+                        name: 'Alliances',
+                        id: 45
+                    },
+                    {
+                        name: 'Customer Experience',
+                        id: 46
+                    },
+                    {
+                        name: 'Sales Engineering',
+                        id: 47
+                    }
+                ],
+                id: 48
+            },
+            {
+                name: 'Services',
+                childDivisions: [
+                    {
+                        name: 'Global Services',
+                        childDivisions: [
+                            {
+                                name: 'Consulting',
+                                childDivisions: [
+                                    {
+                                        name: 'Consulting - APAC',
+                                        id: 49
+                                    },
+                                    {
+                                        name: 'Consulting - EMEA',
+                                        id: 50
+                                    },
+                                    {
+                                        name: 'Consulting - LATAM',
+                                        id: 51
+                                    },
+                                    {
+                                        name: 'Consulting - NA',
+                                        childDivisions: [
+                                            {
+                                                name: 'Consulting - Chicago',
+                                                id: 52
+                                            },
+                                            {
+                                                name: 'Consulting - Los Angeles',
+                                                id: 53
+                                            }
+                                        ]
+                                    }
+                                ],
+                                id: 55
+                            },
+                            {
+                                name: 'Training',
+                                id: 54
+                            }
+                        ],
+                        id: 56
+                    },
+                    {
+                        name: 'Subscription Services',
+                        childDivisions: [
+                            {
+                                name: 'Customer Advocacy',
+                                id: 57
+                            },
+                            {
+                                name: 'Customer Support',
+                                id: 58
+                            },
+                            {
+                                name: 'Enterprise Release',
+                                id: 59
+                            },
+                            {
+                                name: 'Support Operations',
+                                id: 60
+                            },
+                            {
+                                name: 'Technical Support',
+                                id: 61
+                            },
+                            {
+                                name: 'Support - APAC',
+                                id: 62
+                            },
+                            {
+                                name: 'Support - EMEA',
+                                id: 63
+                            },
+                            {
+                                name: 'Support - Iberia',
+                                id: 64
+                            },
+                            {
+                                name: 'Support - Japan',
+                                id: 65
+                            },
+                            {
+                                name: 'Support - Latin America',
+                                id: 66
+                            },
+                            {
+                                name: 'Support - South Asia',
+                                id: 67
+                            },
+                            {
+                                name: 'Support - US',
+                                id: 68
+                            }
+                        ],
+                        id: 69
+                    }
+                ],
+                id: 70
+            }
+        ],
+        id: 1,
+    }
+}
